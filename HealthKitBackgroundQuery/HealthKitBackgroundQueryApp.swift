a6caf7433768f1a5f2d2c32fdc9795198a78b2ff
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-04-14 18:22:29 -0500

    Initial Commit

---
+++ HealthKitBackgroundQuery/HealthKitBackgroundQueryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitBackgroundQueryApp.swift
+//  HealthKitBackgroundQuery
+//
+//  Created by Mark Volkmann on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitBackgroundQueryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
